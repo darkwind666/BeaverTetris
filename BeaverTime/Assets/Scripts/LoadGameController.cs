4141a27e9dcedc3c13205f8d81b7ae8fc98d0114
darkwind666 <darkwind666@mail.ru>
2016-01-09 10:31:07 +0300

    Loading screen ui.
    Make loading screen ui.

---
+++ BeaverTime/Assets/Scripts/LoadGameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadGameController : MonoBehaviour {
+
+    public float time;
+
+    Image circularLoader;
+
+    void Start () {
+
+        circularLoader = GetComponent<Image>();
+        circularLoader.fillAmount = 1f;
+
+    }
+	
+	void Update () {
+
+        if (circularLoader.fillAmount > 0f)
+        {
+
+            circularLoader.fillAmount -= Time.deltaTime / time;
+
+        }
+
+	}
+}
