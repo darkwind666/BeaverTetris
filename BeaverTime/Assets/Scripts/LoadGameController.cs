36d783cd3047a4c9bc07da476e4a72e9c527228b
darkwind666 <darkwind666@mail.ru>
2016-01-10 15:59:38 +0300

    Fade transition between scenes.
    Make fade transition between scenes.

--- BeaverTime/Assets/Scripts/LoadGameController.cs
+++ BeaverTime/Assets/Scripts/LoadGameController.cs
@@ -4,24 +4,26 @@ using UnityEngine.UI;
 public class LoadGameController : MonoBehaviour {
 
     public float time;
+    public FadingScript fadingController;
 
     Image circularLoader;
 
-    void Start () {
-
+    void Start ()
+    {
         circularLoader = GetComponent<Image>();
         circularLoader.fillAmount = 1f;
-
     }
 	
-	void Update () {
-
+	void Update ()
+    {
         if (circularLoader.fillAmount > 0f)
         {
-
             circularLoader.fillAmount -= Time.deltaTime / time;
-
         }
-
+        else
+        {
+            fadingController.startFade("MainMenuScreen", false);
+            enabled = false;
+        }
 	}
 }
