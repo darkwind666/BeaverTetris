963f16c8a1af8e3319c5873f0ed18fc4d40ea279
darkwind666 <darkwind666@mail.ru>
2016-02-22 19:42:38 +0300

    Boss environment.
    Make boss environment.

---
+++ BeaverTime/Assets/Scripts/GameBossesController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameBossesController : MonoBehaviour {
+
+    public GameObject gameBoardPad;
+    public GameObject bossEnvironmentContainer;
+    GameBoard _gameBoard;
+
+    void Start () {
+
+        _gameBoard = ServicesLocator.getServiceForKey(typeof(GameBoard).Name) as GameBoard;
+        createEnvironmentForBoss();
+        createBoss();
+
+    }
+
+    void createEnvironmentForBoss()
+    {
+
+        List<GameObject> children = new List<GameObject>();
+
+        foreach (Transform child in bossEnvironmentContainer.transform)
+        {
+            children.Add(child.gameObject);
+        }
+
+        foreach(GameObject block in children)
+        {
+            Vector3 childPositionOnBoard = block.transform.localPosition;
+            int positionX = (int)Mathf.Round(childPositionOnBoard.x);
+            int positionY = (int)Mathf.Round(childPositionOnBoard.y);
+            _gameBoard.setObjectForXY(block, positionX, positionY);
+            block.transform.SetParent(gameBoardPad.transform, false);
+        }
+
+    }
+
+    void createBoss()
+    {
+
+    }
+
+    void Update () {
+	
+	}
+
+
+}
