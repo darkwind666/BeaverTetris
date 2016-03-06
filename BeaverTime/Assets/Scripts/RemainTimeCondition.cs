d6e1a43e2c46513e4efeebece106118988f1c86d
darkwind666 <darkwind666@mail.ru>
2016-03-06 18:03:19 +0300

    Remain time win condition.
    Make remain time win condition.

---
+++ BeaverTime/Assets/Scripts/RemainTimeCondition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RemainTimeCondition : MonoBehaviour, IWinCondition {
+
+    public int remainTime;
+    int _currentTimeState;
+
+    void Start () {
+
+        _currentTimeState = 0;
+
+    }
+	
+	void Update () {
+	
+	}
+
+    public bool checkWinCondition()
+    {
+        bool win = true;
+
+        _currentTimeState++;
+        if(_currentTimeState < remainTime)
+        {
+            win = false;
+        }
+
+        return win;
+    }
+
+}
