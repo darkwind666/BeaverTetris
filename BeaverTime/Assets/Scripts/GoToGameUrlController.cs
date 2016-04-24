2fac47fb5c47c2c8ea9f85bf9e98cea58d02ed0a
darkwind666 <darkwind666@mail.ru>
2016-04-25 00:26:14 +0300

    Go to windows store and google play buttons.
    Make go to windows store and google play buttons.

---
+++ BeaverTime/Assets/Scripts/GoToGameUrlController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToGameUrlController : MonoBehaviour {
+
+    public string targetUrl;
+
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void goToUrl()
+    {
+        Application.OpenURL(targetUrl);
+    }
+
+}
