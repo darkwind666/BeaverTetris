f1a753b345d5cccf78155e56dce1e4b874f0303d
darkwind666 <darkwinddev@gmail.com>
2016-05-04 00:35:52 +0300

    Block with many lives tutorial.
    Make tutorial about blocks with many lives.

--- BeaverTime/Assets/Scripts/FadingScript.cs
+++ BeaverTime/Assets/Scripts/FadingScript.cs
@@ -125,7 +125,7 @@ public class FadingScript : MonoBehaviour {
         else
         {
             _doFade = false;
-            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadSceneAsync(sceneName);
         }
     }
 
--- BeaverTime/Assets/Scripts/MainGameLogic/MainGameDataLoader.cs
+++ BeaverTime/Assets/Scripts/MainGameLogic/MainGameDataLoader.cs
@@ -9,6 +9,7 @@ public class MainGameDataLoader : MonoBehaviour {
 
     void Awake ()
     {
+        ServicesLocator.loadGameServices();
         GameBoard newGameBoard = new GameBoard(shapeSideInBlocks);
         ServicesLocator.setServiceForKey(newGameBoard, typeof(GameBoard).Name);
 
