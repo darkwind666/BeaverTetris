055579aafb83ff9d57cc8d3c386c622d34f78aaa
darkwinddev <darkwinddev@gmail.comgit config --global user.email arteomchik@gmail.com>
2016-07-22 14:11:11 +0300

    add game start vk pop up

--- BeaverTime/Assets/GameFiles/Scripts/MainMenuController.cs
+++ BeaverTime/Assets/GameFiles/Scripts/MainMenuController.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using com.playGenesis.VkUnityPlugin;
+using com.playGenesis.VkUnityPlugin.MiniJSON;
+using System;
+using System.Collections.Generic;
+using System.Collections;
 
 public class MainMenuController : MonoBehaviour {
 
@@ -7,6 +12,7 @@ public class MainMenuController : MonoBehaviour {
 
     public GameObject selectLevelButton;
     public GameObject soundSettingsPopUp;
+	public GameObject vkLogInPopUp;
 	public GameObject endlessLevelButton;
 
     public Slider musicSlider;
@@ -15,22 +21,30 @@ public class MainMenuController : MonoBehaviour {
     public AudioSource buttonClickEffect;
     public AudioSource backgroundSound;
 
-    GamePlayerDataController _playerData;
-
     public string selectLevelSceneName;
 	public string endlessLevelSceneName;
+	public GameGlobalSettings gameGlobalSettings;
+
+	GamePlayerDataController _playerData;
+	VkApi _vkapi;
 
     void Start () {
 
         _playerData = ServicesLocator.getServiceForKey(typeof(GamePlayerDataController).Name) as GamePlayerDataController;
+		_vkapi = VkApi.VkApiInstance;
+		_vkapi.LoggedIn += onVKLogin;
 
         if (_playerData.playerExist == false) {
             selectLevelButton.SetActive(false);
 			endlessLevelButton.SetActive(false);
         }
 
-        setupAudio();
+		// _vkapi.IsUserLoggedIn == false &&
+		if (_playerData.playerExist == false) {
+			vkLogInPopUp.SetActive (true);
+		} 
 
+        setupAudio();
     }
 
     void setupAudio()
@@ -42,6 +56,10 @@ public class MainMenuController : MonoBehaviour {
         buttonClickEffect.volume = _playerData.gameSoundEffectsVolume;
     }
 
+	void OnDisable()
+	{
+		_vkapi.LoggedIn -= onVKLogin;
+	}
 
     void Update () {
 	
@@ -85,5 +103,63 @@ public class MainMenuController : MonoBehaviour {
         _playerData.gameSoundEffectsVolume = aVolume;
         _playerData.savePlayerData();
     }
+
+
+	public void logIn()
+	{
+		_vkapi.Login();
+	}
+
+	void onVKLogin()
+	{
+		VKRequest r = new VKRequest
+		{
+			url="users.get?",
+			CallBackFunction=OnGetUserInfo
+		};
+		_vkapi.Call (r);
+	}
+
+	public void OnGetUserInfo (VKRequest request)
+	{
+		if(request.error!=null)
+		{
+			return;
+		}
+
+		var dict=Json.Deserialize(request.response) as Dictionary<string,object>;
+		var users=(List<object>)dict["response"];
+		var user = VKUser.Deserialize (users [0]);
+		string fullPlayerName = user.first_name + " " + user.last_name;
+		_playerData.createNewPlayerWithName (fullPlayerName);
+		_playerData.playerScore += gameGlobalSettings.logInReward;
+		_playerData.logInVk = true;
+		_playerData.savePlayerData();
+		selectLevelButton.SetActive(true);
+		endlessLevelButton.SetActive(true);
+		vkLogInPopUp.SetActive(false);
+		getPlayerRewardForGroup ();
+	}
+
+	void getPlayerRewardForGroup()
+	{
+		VKRequest r1 = new VKRequest (){
+			url="groups.isMember?group_id=" + gameGlobalSettings.vkGameGroupId,
+			CallBackFunction=onPlayerRewardForGroup
+		};
+		_vkapi.Call (r1);
+	}
+
+	void onPlayerRewardForGroup(VKRequest request)
+	{
+		var dict = Json.Deserialize(request.response) as Dictionary<string,object>;
+		bool inGroup = Convert.ToBoolean(dict ["response"]);
+
+		if (inGroup) {
+			_playerData.playerScore += gameGlobalSettings.joinGroupReward;
+			_playerData.inVkGameGroup = true;
+			_playerData.savePlayerData();
+		}
+	}
 
 }
